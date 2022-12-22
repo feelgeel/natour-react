61bf9c4c817ce2abfb96d863f17bc40fb3f7c7ad
cisco-home-pc <feelgeel@gmail.com>
2022-12-22 20:28:07 +0100

    make it online

---
+++ natour/features/Features.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useState } from 'react'
+import Box from '../../component/box/Box'
+// const [boxes,seboxes]=useState([
+//     {id:1},
+//     {id:2},
+//     {id:3},
+//     {id:4}
+// ])
+const Features=()=>{
+  return (
+    <div className='features'>
+        <Box/>
+    </div>
+  )
+}
+export default Features
