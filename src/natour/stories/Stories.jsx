c20643932161180fac51aeebea62615a3397e157
cisco-laptop <feelgeel@gmail.com>
2022-12-25 17:31:20 +0100

    stories

--- src/natour/stories/Stories.jsx
+++ src/natour/stories/Stories.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import vidmp4 from "../img/video.mp4"
 import vidwebm from "../img/video.webm"
+import Story from "../story/Story"
 export default function Stories() {
     return (
         <div className='stories'>
-            <div class="bg-video">
-            <video  class="bg-video__content"  autoPlay muted loop>
+            <h2 className="h-secondary">we make people geniunely happy</h2>
+            <Story/>
+            <Story/>
+            {/* <Story/>
+            <Story/> */}
+            {/* <div className="bg-video">
+            <video  className="bg-video__content"  autoPlay muted loop>
             <source src={vidmp4} type="video/mp4"/>
             <source src={vidwebm} type="video/webm"/>
             </video>
-            </div>
+            </div> */}
         </div>
     )
 }
--- src/natour/story/Story.jsx
+++ src/natour/story/Story.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import st_img from "../img/nat-8.jpg"
 
 export default function Story() {
   return (
-    <div>
-      
+    <div className="story">
+      <div className="story-shape">
+        <img src={st_img} alt="" className="story-img"/>
+        <span className="story-caption">
+          mary smith
+        </span>
+      </div>
+      <div className="story-info">
+        <h3 className="story-heading">i had a good time</h3>
+        <p className="story-p">
+          Lorem ipsum, dolor sit amet consectetur adipisicing
+          elit. Veniam, recusandae impedit praesentium
+          *elit. Veniam, recusandae impedit praesentium
+          *elit. Veniam, recusandae impedit praesentium
+          *elit. Veniam, recusandae impedit praesentium
+
+
+      </p>
+      </div>
     </div>
   )
 }
